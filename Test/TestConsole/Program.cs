a0826af27c534eb119009f1a4a8a7ffc3e1904fd
Anton <an.kr.working@gmail.com>
2017-10-18 20:31:42 +0300

    gf

---
+++ Test/TestConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string text = client.DownloadString(@"http://resources.finance.ua/ua/public/currency-cash.json");
+
+            Console.WriteLine(text);
+        }
+    }
+}
